e08d469a7fe5587d1bec4cc16e6595dc8e3df67e
Andrew huggins <andrewhuggins@gmail.com>
2016-10-08 11:31:13 -0400

    updating widget provider/alias

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -35,7 +35,7 @@ class InstallCommand extends Command
         ArtisanCMS\Blog\Providers\BlogServiceProvider::class,
         GrahamCampbell\Markdown\MarkdownServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Arrilot\Widgets\ServiceProvider::class,
+        ArtisanCMS\Widgets\ServiceProvider::class,
 
 SERVICE_PROVIDERS;
         // the blank line is needed for consistency
@@ -45,8 +45,8 @@ SERVICE_PROVIDERS;
         'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Widget'       => Arrilot\Widgets\Facade::class,
-        'AsyncWidget'  => Arrilot\Widgets\AsyncFacade::class,
+        'Widget'       => ArtisanCMS\Widgets\Facade::class,
+        'AsyncWidget'  => ArtisanCMS\Widgets\AsyncFacade::class,
 
 ALIASES;
 
